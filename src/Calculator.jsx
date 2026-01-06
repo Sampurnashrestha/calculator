0a040af42bb5dae673967247e804cfc5b15efba3
Sampurnashrestha <shresthasampurna849@gmail.com>
2026-01-06 18:38:50 +0545

    first commit

---
+++ src/Calculator.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Calculator = () => {
+  return (
+    <>
+    <div className=''>
+        <div >0</div>
+
+        <div>
+            <button>C</button>
+            <button>Del</button>
+            <button>%</button>
+            <button></button>
+        </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Calculator
